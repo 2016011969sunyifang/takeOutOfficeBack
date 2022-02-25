4d0455d1fcef7dbf52fc6d62b39e10de10886f37
Sun <Yifang.Sun@connext.com.cn>
2022-02-25 18:25:33 +0800

    更新readme

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,13 +2,13 @@ module.exports = {
   ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [2, 'always'],
+    'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 108],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
+    'header-max-length': [1, 'always', 108],
+    'subject-empty': [1, 'never'],
+    'type-empty': [1, 'never'],
     'type-enum': [
-      2,
+      1,
       'always',
       [
         'feat',
